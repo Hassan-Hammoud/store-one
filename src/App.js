2105b503b2981b99136c612c587b7e03548da023
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-01-04 16:16:48 +0200

    finished header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Store from './components/Store/Store';
 import About from './components/About/About';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
+
+    <>
+    <Navbar />
     <Container  className='mb-4'> 
     <Routes>
       <Route path='/' element={<Home/>}/>
@@ -14,6 +18,7 @@ function App() {
       <Route path='/about' element={<About/>}/>
     </Routes>
     </Container>
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar as NavbarBootStrap,
+} from "react-bootstrap";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <NavbarBootStrap sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/about">
+            About
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/store">
+            Store
+          </Nav.Link>
+        </Nav>
+        <Button
+          variant="outline-primary"
+          className="rounded-circle"
+          style={{ fontSize: "20px", position: "relative" }}
+        >
+          <i class="bi bi-cart3 "></i>
+
+          <div
+            className="d-flex justify-content-center align-items-center bg-danger rounded-circle"
+            style={{
+              position: "absolute",
+              color: "white",
+              width: "20px",
+              height: "20px",
+              bottom: "0",
+              right: "0",
+              transform: "translate(25%,25%)",
+              fontSize: "14px",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavbarBootStrap>
+  );
+};
+
+export default Navbar;
