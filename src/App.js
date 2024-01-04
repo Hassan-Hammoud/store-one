c79de70158840e3a74b1a74534dc0f9697f209db
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-01-04 13:34:05 +0200

    Setup the three main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Store from './components/Store/Store';
+import About from './components/About/About';
 
 function App() {
   return (
-    <div className="App">
-          <h1>Hello Store One</h1>
-    </div>
+    <Container  className='mb-4'> 
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/store' element={<Store/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+    </Container>
   );
 }
 
---
+++ src/components/Store/Store.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Store.css'
+const Store = () => {
+  return (
+    <div>
+      store
+    </div>
+  )
+}
+
+export default Store
